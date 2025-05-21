dc1dc86c81e2822a556bcdbcd50bf3199e6afbdc
matheusfdosan <matheusfaustinoe20@gmail.com>
2025-05-21 12:19:27 -0300

    matheusfdosan: decreasing the sidebar

--- client/src/components/LateralBar/index.jsx
+++ client/src/components/LateralBar/index.jsx
@@ -11,66 +11,156 @@ import howWorksIcon from "../../assets/LateralBarIcons/how-works-outlined.svg"
 import aboutIcon from "../../assets/LateralBarIcons/about-outlined.svg"
 import arrowIcon from "../../assets/LateralBarIcons/inclined-arrow-normal.svg"
 
+import homeIconActive from "../../assets/LateralBarIcons/home-filled.svg"
+import requestIconActive from "../../assets/LateralBarIcons/requests-filled.svg"
+import newRequestIconActive from "../../assets/LateralBarIcons/new-request-filled.svg"
+import documentsIconActive from "../../assets/LateralBarIcons/documents-filled.svg"
+import supportIconActive from "../../assets/LateralBarIcons/support-filled.svg"
+import configsIconActive from "../../assets/LateralBarIcons/configs-filled.svg"
+import howWorksIconActive from "../../assets/LateralBarIcons/how-works-filled.svg"
+import aboutIconActive from "../../assets/LateralBarIcons/about-filled.svg"
+import arrowIconActive from "../../assets/LateralBarIcons/inclined-arrow-blue.svg"
+
+import { useNavigation } from "../../contexts/NavigationContext"
+
 function LateralBar() {
+  const { active, setActive } = useNavigation()
+
+  const handleClickOption = (opt) => {
+    setActive(opt)
+  }
+
   return (
     <nav id="lateral-bar">
       <ul>
-        <li>
-          <a href="/">
-            <img src={homeIcon} alt="" />
+        <li
+          style={{
+            backgroundColor:
+              active === "home" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("home")}>
+            <img
+              src={active === "home" ? homeIconActive : homeIcon}
+              alt="home-icon"
+            />
             <span>Home</span>
           </a>
         </li>
-        <li>
-          <a href="/">
-            <img src={requestIcon} alt="" />
+        <li
+          style={{
+            backgroundColor:
+              active === "your-requests" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("your-requests")}>
+            <img
+              src={active === "your-requests" ? requestIconActive : requestIcon}
+              alt="requests-icon"
+            />
             <span>Suas Solicitações</span>
           </a>
         </li>
-        <li>
-          <a href="/">
-            <img src={newRequestIcon} alt="" />
+        <li
+          style={{
+            backgroundColor:
+              active === "new-request" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("new-request")}>
+            <img
+              src={
+                active === "new-request" ? newRequestIconActive : newRequestIcon
+              }
+              alt="new-request-icon"
+            />
             <span>Nova Solicitação</span>
           </a>
         </li>
-        <li>
-          <a href="/">
-            <img src={documentsIcon} alt="" />
+        <li
+          style={{
+            backgroundColor:
+              active === "documents" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("documents")}>
+            <img
+              src={active == "documents" ? documentsIconActive : documentsIcon}
+              alt="documents-icon"
+            />
             <span>Documentos Fiscais</span>
           </a>
         </li>
         <li id="lateral-bar-line"></li>
-        <li>
-          <a href="/">
-            <img src={supportIcon} alt="" />
+        <li
+          style={{
+            backgroundColor:
+              active === "support" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("support")}>
+            <img
+              src={active === "support" ? supportIconActive : supportIcon}
+              alt="support"
+            />
             <span>Suporte (Ajuda)</span>
           </a>
         </li>
-        <li>
-          <a href="/">
-            <img src={configsIcon} alt="" />
+        <li
+          style={{
+            backgroundColor:
+              active === "settings" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("settings")}>
+            <img
+              src={active === "settings" ? configsIconActive : configsIcon}
+              alt="settings-icon"
+            />
             <span>Configurações</span>
           </a>
         </li>
       </ul>
 
       <ul>
-        <li>
-          <a href="/">
+        <li
+          style={{
+            backgroundColor:
+              active === "about" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("about")}>
             <div>
-              <img src={aboutIcon} alt="" />
+              <img
+                src={active === "about" ? aboutIconActive : aboutIcon}
+                alt="about-icon"
+              />
               <span>Sobre Nós</span>
             </div>
-            <img src={arrowIcon} alt="" />
+            <img
+              src={active === "about" ? arrowIconActive : arrowIcon}
+              alt="arrow"
+            />
           </a>
         </li>
-        <li>
-          <a href="/">
+        <li
+          style={{
+            backgroundColor:
+              active === "how-works" && "var(--primary-color-more-clearer)",
+          }}
+        >
+          <a onClick={() => handleClickOption("how-works")}>
             <div>
-              <img src={howWorksIcon} alt="" />
+              <img
+                src={active === "how-works" ? howWorksIconActive : howWorksIcon}
+                alt="how-works-icon"
+              />
               <span>Como funciona?</span>
             </div>
-            <img src={arrowIcon} alt="" />
+            <img
+              src={active === "how-works" ? arrowIconActive : arrowIcon}
+              alt="arrow"
+            />
           </a>
         </li>
       </ul>
