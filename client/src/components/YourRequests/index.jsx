635e91569150cd40c11df337bb6ff27e43031daa
matheusfdosan <matheusfaustinoe20@gmail.com>
2025-05-27 21:26:48 -0300

    matheusfdosan: filter requests

--- client/src/components/YourRequests/index.jsx
+++ client/src/components/YourRequests/index.jsx
@@ -5,15 +5,56 @@ import getRequests from "../../utils/getRequests.js"
 
 function YourRequests() {
   const [requests, setRequests] = useState([])
+  const [filteredRequests, setFilteredRequests] = useState([])
+  const [filter, setFilter] = useState({
+    status: "Todos",
+    city: "",
+  })
 
-  const getRequestsFunc = async () => {
-    return await getRequests(JSON.parse(localStorage.getItem("login")).auth.id)
+  const handleChange = ({ target }) => {
+    const { name, value } = target
+    setFilter((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleFilterButton = () => {
+    if (filter.status === "Todos" && filter.city === "") {
+      setFilteredRequests(requests)
+      return
+    }
+
+    const filteredRequests = requests.filter((reqs) => {
+      if (reqs.status === "Todos") {
+        return (
+          reqs.origin_city === filter.city ||
+          reqs.destination_city === filter.city
+        )
+      }
+      return (
+        reqs.status === filter.status ||
+        reqs.origin_city === filter.city ||
+        reqs.destination_city === filter.city
+      )
+    })
+
+    setFilteredRequests(filteredRequests)
+  }
+
+  const handleCleanFilter = () => {
+    setFilter((prev) => ({
+      ...prev,
+      city: "",
+    }))
+    setFilteredRequests(requests)
   }
 
   useEffect(() => {
     const fetchRequests = async () => {
-      const result = await getRequestsFunc()
+      const result = await getRequests(
+        JSON.parse(localStorage.getItem("login")).auth.id
+      )
+
       setRequests(result)
+      setFilteredRequests(result)
     }
 
     fetchRequests()
@@ -25,49 +66,55 @@ function YourRequests() {
 
       <div id="filter">
         <div id="filter-inputs">
-          <label htmlFor="input-status">
+          <label htmlFor="status">
             <h3>Status</h3>
-            <select name="input-status" id="input-status">
+            <select name="status" id="status" onChange={handleChange}>
               <option value="Todos">Todos</option>
-              <option value="Em Andamento">Em Andamento</option>
-              <option value="Pendente">Pendente</option>
-              <option value="Conclída">Concluída</option>
-              <option value="Cancelada">Cancelada</option>
+              <option value="in-progress">Em Andamento</option>
+              <option value="pending">Pendente</option>
+              <option value="completed">Concluída</option>
+              <option value="canceled">Cancelada</option>
             </select>
           </label>
-          <label htmlFor="input-local">
+          <label htmlFor="city">
             <h3>Cidade/UF</h3>
             <input
               type="text"
-              name="input-local"
-              id="input-local"
+              name="city"
+              id="city"
               placeholder="Ex: São Paulo, SP"
+              onChange={handleChange}
+              value={filter.city}
             />
           </label>
         </div>
 
         <div id="filter-buttons">
-          <button>Limpar</button>
-          <button>Aplicar Filtros</button>
+          <button onClick={handleCleanFilter}>Limpar</button>
+          <button onClick={handleFilterButton}>Aplicar Filtros</button>
         </div>
       </div>
 
       <div id="horizontal-line"></div>
 
       <div id="requests-container">
-        {requests.length !== 0 ? requests.map((request, index) => (
-          <Request
-            key={index}
-            id={request.id}
-            status={request.status}
-            local={{
-              origin: request.origin_city,
-              destination: request.destination_city,
-            }}
-            type={request.type}
-            value={request.estimated_shipping_cost}
-          />
-        )) : <p id="no-requests">Você não fez nenhuma solicitação!</p>}
+        {filteredRequests.length !== 0 ? (
+          filteredRequests.map((request, index) => (
+            <Request
+              key={index}
+              id={request.id}
+              status={request.status}
+              local={{
+                origin: request.origin_city,
+                destination: request.destination_city,
+              }}
+              type={request.type}
+              value={request.estimated_shipping_cost}
+            />
+          ))
+        ) : (
+          <p id="no-requests">Você não fez nenhuma solicitação!</p>
+        )}
       </div>
     </div>
   )
